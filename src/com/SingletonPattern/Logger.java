79f0c27a89d08367696abcd8417573e98453ee81
Rishav Ghising <ghisingrishav@gmail.com>
2024-07-18 20:19:46 +0545

    singleton pattern

---
+++ src/com/SingletonPattern/Logger.java
@@ -0,0 +1,33 @@
+package com.SingletonPattern;
+
+public class Logger {
+
+// singleton class implementation rsvgsng
+
+        /*
+            Creating a private constructor to prevent the creation of objects from outside the class
+            this is the first step to implement the singleton pattern!
+         */
+        private  Logger(){}
+
+//    Creating a private static instance of the class this holds the only instance of the class its like a bucket to keep the instance of the class and by default the value is null.
+        private  static  Logger logger;
+
+        public  static  Logger  getLogger(){
+            /*
+                1. Check if the instance doesnot exist already
+                2. If it does not exist, create a new instance
+                3. Return the newly created instance
+             */
+            if(logger==null){
+                logger = new Logger();
+                return  logger;
+            }
+            return  logger;
+        }
+
+        public void log(String message){
+            System.out.println(message);
+        }
+
+}
--- src/com/SingletonPattern/main.java
+++ src/com/SingletonPattern/main.java
@@ -1,7 +1,16 @@
 package com.SingletonPattern;
 
+//This demonstrates the Singleton Pattern
 public class main {
     public static void main(String[] args) {
 
+        Logger l = Logger.getLogger();
+        Logger l2 = Logger.getLogger();
+
+        System.out.println(l.hashCode());
+        System.out.println(l2.hashCode());
+
+        l.log("Logging with l as the instance");
+        l2.log("Logging with l2 as the same instance of the class");
     }
 }
